908ebc958c94d67a661b7c64e3a9fb915c2f2d76
Jeferson <jejoalca14@gmail.com>
2019-09-12 12:33:23 -0400

    Agregar imagenes a slider

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -36,11 +36,38 @@ $(document).ready(function(){
 	init();
 })
 
-function showModal(param1, param2){
+function showModal(param1, param2, id){
+
 	let img = document.getElementById('img');
 	let pn = document.getElementById('project-name');
 	let purl = document.getElementById('project-url');
 	img.src = param1.toString();
 	pn.innerHTML = param2.toString();
 	purl.innerHTML = "www." + param2.toString().replace(" ", "") + ".com"
+
+	let carousel_container = $('#carousel-container') 
+
+	let carousel_item = '<div class="carousel-item"><img id="img" class="d-block w-100" src="assets/image/5.png"></div>'
+
+	carousel_container.append(carousel_item);
+	carousel_container.append(carousel_item);
+	carousel_container.append(carousel_item);
+
+	// Traerte este elemento <div class="carousel-inner"> con jquery e insertar div que forman parte del carousel 
+	// Tienen la forma 
+	//<div class="carousel-item">
+	//	<img id="img" class="d-block w-100" src="assets/image/5.png" alt="First slide">
+	//</div>
+	
+
+	// For para crear div dentro del carousel o slider de la forma
+
+	//<div class="carousel-item">
+	//	<img id="img" class="d-block w-100" src="assets/image/5.png" alt="First slide">
+	//</div>
+
+		// Crear tag div del carousel 
+		// Añadir clase carousel-item
+		// Insertar dentro del div un tag img con el src 'assets/image/1.png' (imagen)
+
 }
